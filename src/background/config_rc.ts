c5839c59b960e315e16aee3a7dc0f537d322e969
faefeyfa <faefeyfa@protonmail.com>
2023-01-24 19:13:10 -0600

    Revert "remove comment lines before joining ex commands"
    This reverts commit c41ddbf74c642200233d57e87d6d652628f3876b.

--- src/background/config_rc.ts
+++ src/background/config_rc.ts
@@ -59,6 +59,12 @@ export async function runRc(rc: string) {
 }
 
 export function rcFileToExCmds(rcText: string): string[] {
+    // string-join lines that end with /
+    const joined = rcText.replace(/\\\n/g, "")
+
+    // Split into individual excmds
+    const excmds = joined.split("\n")
+
     // Remove empty and comment lines
     return excmds.filter(
         x =>
@@ -66,10 +72,4 @@ export function rcFileToExCmds(rcText: string): string[] {
             !x.trim().startsWith('"') &&
             !x.trim().startsWith("#"),
     )
-
-    // string-join lines that end with /
-    const joined = rcText.replace(/\\\n/g, "")
-
-    // Split into individual excmds
-    const excmds = joined.split("\n")
 }
